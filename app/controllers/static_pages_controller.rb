dea51f19e54f60b6630b5eb7271e8dae569e108a
Josefine Rost <nijrost@gmail.com>
2017-10-03 21:17:50 +0200

    Add contact page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-    @message = "David är bäst!"
+    @message = 'David är bäst!'
+  end
+
+  def contact
+    @name =  'Börel'
+    @email = 'böreljörge@gmail.com'
+    @phone = '1234567'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get('', controller: :static_pages, action: :home)
+  get(:contact, controller: :static_pages, action: :contact)
 end
