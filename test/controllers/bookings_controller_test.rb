8589b0b6c020f0da8dfa71afcde301e67bde8f3f
David Wessman <david.wesmn@gmail.com>
2018-01-30 21:10:23 +0100

    Setup testing with Clearance

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -1,53 +1,52 @@
 require 'test_helper'
 
 class BookingsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    user = User.create(name: 'Josefine', email: 'jos@best.se')
-    @booking = Booking.create(start: 2.hours.from_now,
-                              stop: 4.hours.from_now,
-                              user: user)
-  end
-
   test 'should get index' do
+    bookings(:first)
     get(bookings_url)
     assert_response(:success)
   end
 
   test 'should get new' do
+    sign_in_as(users(:josefine))
     get(new_booking_url)
     assert_response(:success)
   end
 
   test 'should create booking' do
-    assert_difference('Booking.count') do
-      post(bookings_url,
-           params: {
-             booking: { start: @booking.start,
-                        stop: @booking.stop,
-                        user_id: @booking.user_id }
-           })
+    user = users(:josefine)
+    sign_in_as(user)
+    assert_difference('Booking.count', 1) do
+      post(bookings_url, params: { booking: { start: 4.hours.from_now,
+                                              stop: 6.hours.from_now } })
     end
 
     assert_redirected_to(bookings_url)
+    assert_equal(Booking.last.user, user)
   end
 
   test 'should get edit' do
-    get(edit_booking_url(@booking))
+    booking = bookings(:first)
+    sign_in_as(booking.user)
+    get(edit_booking_url(booking))
     assert_response(:success)
   end
 
   test 'should update booking' do
-    patch(booking_url(@booking),
-          params: {
-            booking: { start: @booking.start,
-                       stop: @booking.stop }
-          })
+    booking = bookings(:first)
+    sign_in_as(booking.user)
+    start = Time.current
+    patch(booking_url(booking), params: { booking: { start: start } })
     assert_redirected_to(bookings_url)
+    booking.reload
+    assert_in_delta(booking.start, start, 1.second)
   end
 
   test 'should destroy booking' do
+    booking = bookings(:first)
+    sign_in_as(booking.user)
     assert_difference('Booking.count', -1) do
-      delete(booking_url(@booking))
+      delete(booking_url(booking))
     end
 
     assert_redirected_to(bookings_url)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,21 @@
+# frozen_string_literal: true
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'clearance/test_unit'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+end
 
-  # Add more helper methods to be used by all tests here...
+class ActionDispatch::IntegrationTest
+  def sign_in_as(user)
+    post(session_url, params: { session: { email: user.email,
+                                           password: 'passpass' } })
+  end
+
+  def sign_out
+    delete(sign_out_url)
+  end
 end
