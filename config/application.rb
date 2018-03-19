6fd8e0c90f545073dd6cdaed364c346be1d0c5bc
David Wessman <david.wesmn@gmail.com>
2018-03-19 21:57:17 +0100

    I18n configuration

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module Tidsbokning
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.default_locale = :sv
+    config.i18n.available_locales = [:sv, :en]
+    config.i18n.default_locale = :sv
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
