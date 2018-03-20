904962d034d96509b2b62b20c97ee30327fcbf68
David Wessman <david.wesmn@gmail.com>
2018-03-20 17:51:15 +0100

    Rails 5.2.0

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,17 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+# Pick the frameworks you want:
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+# require 'active_job/railtie'
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
+# require 'action_cable/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +20,7 @@ Bundler.require(*Rails.groups)
 module Tidsbokning
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
     config.i18n.available_locales = [:sv, :en]
     config.i18n.default_locale = :en
 
