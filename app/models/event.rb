45d70ab1a04a8e996f164f34f36695e4854036f1
David Wessman <david.wesmn@gmail.com>
2018-03-20 22:10:11 +0100

    Add image to event!

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to(:user)
+  has_one_attached(:image)
   validates(:title, :start, :stop, :location, :price, presence: true)
   validates(:price, numericality: { greater_than_or_equal_to: 0 })
 
